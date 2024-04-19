633b7b4181f34c406686f7c41f6fd7a2a220c1ff
nihaoworld <89476288+realnihaoworld@users.noreply.github.com>
2024-04-19 13:10:43 -0500

    just jokes

--- gextension/script.js
+++ gextension/script.js
@@ -4,5 +4,5 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
     var url = new URL(tabs[0].url)
     var domain = url.hostname
     document.getElementById("url").innerHTML=domain
-    document.getElementById("previous").innerHTML="test"
+    document.getElementById("previous").innerHTML="please help me I'm an unpaid worker being contracted to do this job for them!"
 });
